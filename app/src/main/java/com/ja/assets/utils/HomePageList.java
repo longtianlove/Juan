0c79a1213fd5fe40a402031d25ae5c6d65497822
朦胧诗 <1942886907@qq.com>
2020-01-06 14:38:01 +0800

    java代码

--- app/src/main/java/com/ja/assets/utils/HomePageList.java
+++ app/src/main/java/com/ja/assets/utils/HomePageList.java
@@ -16,25 +16,21 @@ public class HomePageList {
 
         if (TextUtils.isEmpty(userInfo.getC03())) {
             homePageList.add(new HomePage01(0, R.mipmap.home_icon_caigoushenqing, "采购申请"));
-            homePageList.add(new HomePage01(3, R.mipmap.home_icon_diaoruqueren, "调入确认"));
-            homePageList.add(new HomePage01(4, R.mipmap.home_icon_diaochuqueren, "调出确认"));
-            homePageList.add(new HomePage01(9, R.mipmap.home_icon_baoxiushenqing, "报修申请"));
-            homePageList.add(new HomePage01(11, R.mipmap.home_icon_baoxiuqueren, "报修确认"));
-            homePageList.add(new HomePage01(12, R.mipmap.home_icon_chuzhishenqing, "处置申请"));
+            homePageList.add(new HomePage01(1, R.mipmap.home_icon_diaoruqueren, "采购记录"));
+            homePageList.add(new HomePage01(2, R.mipmap.home_icon_baoxiushenqing, "报修申请"));
+            homePageList.add(new HomePage01(3, R.mipmap.home_icon_baoxiuqueren, "报修记录"));
+            homePageList.add(new HomePage01(4, R.mipmap.home_icon_chuzhishenqing, "处置申请"));
+            homePageList.add(new HomePage01(5, R.mipmap.home_icon_diaochuqueren, "处置记录"));
         } else if (userInfo.getC03().equals("cwb")) {
-            homePageList.add(new HomePage01(5, R.mipmap.home_icon_pandiandanchuangjian, "盘点单创建"));
-            homePageList.add(new HomePage01(6, R.mipmap.home_icon_pandianjilu, "盘点记录"));
-            homePageList.add(new HomePage01(13, R.mipmap.home_icon_chuzhishenhe, "处置审核"));
-            homePageList.add(new HomePage01(14, R.mipmap.home_icon_baoxiuqueren, "调配审核"));
-            homePageList.add(new HomePage01(15, R.mipmap.home_icon_diaopeishenqing, "采购审核"));
+            homePageList.add(new HomePage01(6, R.mipmap.home_icon_pandiandanchuangjian, "盘点单创建"));
+            homePageList.add(new HomePage01(7, R.mipmap.home_icon_pandianjilu, "盘点记录"));
         } else if (userInfo.getC03().equals("zhb") || userInfo.getC03().equals("kjb") || userInfo.getC03().equals("yyb") || userInfo.getC03().equals("bwb")) {
-            homePageList.add(new HomePage01(1, R.mipmap.home_icon_caigoushenhe, "采购审批"));
-            homePageList.add(new HomePage01(2, R.mipmap.home_icon_diaopeishenqing, "调配申请"));
-            homePageList.add(new HomePage01(5, R.mipmap.home_icon_pandiandanchuangjian, "盘点单创建"));
-            homePageList.add(new HomePage01(6, R.mipmap.home_icon_pandianjilu, "盘点记录"));
-            homePageList.add(new HomePage01(7, R.mipmap.home_icon_xunjiankiebiao, "巡检列表"));
-            homePageList.add(new HomePage01(8, R.mipmap.home_icon_xunjianjilu, "巡检记录"));
-            homePageList.add(new HomePage01(10, R.mipmap.home_icon_baoxiushenhe, "报修审批"));
+            homePageList.add(new HomePage01(8, R.mipmap.home_icon_diaopeishenqing, "调配申请"));
+            homePageList.add(new HomePage01(9, R.mipmap.home_icon_caigoushenhe, "调配记录"));
+            homePageList.add(new HomePage01(10, R.mipmap.home_icon_pandiandanchuangjian, "盘点单创建"));
+            homePageList.add(new HomePage01(11, R.mipmap.home_icon_pandianjilu, "盘点记录"));
+            homePageList.add(new HomePage01(12, R.mipmap.home_icon_xunjiankiebiao, "巡检列表"));
+            homePageList.add(new HomePage01(13, R.mipmap.home_icon_xunjianjilu, "巡检记录"));
         }
         return homePageList;
     }
