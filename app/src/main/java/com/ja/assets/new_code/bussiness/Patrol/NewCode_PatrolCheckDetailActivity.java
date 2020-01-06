b2fb5daa15eb33d07876084c62913ee9f8a51fff
龙 <bingodongtian@gmail.com>
2020-01-06 19:27:29 +0800

    填写巡检信息

---
+++ app/src/main/java/com/ja/assets/new_code/bussiness/Patrol/NewCode_PatrolCheckDetailActivity.java
@@ -0,0 +1,56 @@
+package com.ja.assets.new_code.bussiness.Patrol;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.ja.assets.R;
+import com.ja.assets.glide.GlideImgUtils;
+import com.ja.assets.ui.base.BaseJavaActivity;
+import com.ja.assets.utils.PhotoUtils;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+import me.nereo.multi_image_selector.MultiImageSelectorActivity;
+
+
+public class NewCode_PatrolCheckDetailActivity extends Activity {
+
+    View ll_tijiaotupian;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.newcode_activity_inspection_detail);
+        initView();
+    }
+
+    void initView() {
+        ll_tijiaotupian = findViewById(R.id.ll_tijiaotupian);
+        ll_tijiaotupian.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new PhotoUtils().pickImage(NewCode_PatrolCheckDetailActivity.this,1,0);
+            }
+        });
+    }
+
+
+        @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data == null && resultCode != RESULT_OK) {
+            return;
+        }
+        switch (requestCode) {
+            case 0:
+                List<String> photoList1 = data.getStringArrayListExtra(MultiImageSelectorActivity.EXTRA_RESULT);
+                GlideImgUtils.loadImage(this, photoList1.get(0), R.mipmap.add_img, findViewById(R.id.lossReportImg));
+                break;
+        }
+    }
+
+}
