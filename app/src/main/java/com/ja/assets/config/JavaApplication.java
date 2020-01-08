02aa914453813cf500bd82f36766a9a9dcebc9b9
龙 <bingodongtian@gmail.com>
2020-01-08 09:23:59 +0800

    temp

--- app/src/main/java/com/ja/assets/config/JavaApplication.java
+++ app/src/main/java/com/ja/assets/config/JavaApplication.java
@@ -3,11 +3,15 @@ package com.ja.assets.config;
 
 import android.app.Application;
 
+import com.tencent.bugly.Bugly;
+
 public class JavaApplication extends MyApplication {
     public static Application mcontext;
     @Override
     public void onCreate() {
         super.onCreate();
         mcontext=this;
+
+        Bugly.init(getApplicationContext(), "cd5e8caa91", false);
     }
 }
