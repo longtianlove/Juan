95bd2f9f4ce9c4900d2a9dfc9e9a2bc1bc24d600
龙 <bingodongtian@gmail.com>
2020-01-12 17:30:36 +0800

    temp

---
+++ app/src/main/java/com/ja/assets/new_code/bussiness/diaopei/DiaopeiBumenActivity.java
@@ -0,0 +1,17 @@
+package com.ja.assets.new_code.bussiness.diaopei;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.ja.assets.R;
+
+public class DiaopeiBumenActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_diaopeishenqing);
+    }
+}
