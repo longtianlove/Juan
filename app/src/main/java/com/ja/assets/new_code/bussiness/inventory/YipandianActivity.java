8a9ec9a2198d49856ebe56a865a4638c3b7d2c0d
龙 <bingodongtian@gmail.com>
2020-01-15 09:04:58 +0800

    沉浸式title修改

--- app/src/main/java/com/ja/assets/new_code/bussiness/inventory/YipandianActivity.java
+++ app/src/main/java/com/ja/assets/new_code/bussiness/inventory/YipandianActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
@@ -14,6 +15,7 @@ import androidx.annotation.Nullable;
 
 import com.ja.assets.R;
 import com.ja.assets.new_code.base.BaseBean;
+import com.ja.assets.new_code.base.BaseJavaActivity;
 import com.ja.assets.new_code.bussiness.bean.post.ChuangjianpandiandanBean;
 import com.ja.assets.new_code.bussiness.bean.post.WeiPandianPostBean;
 import com.ja.assets.new_code.bussiness.bean.result.WeiPandianResultBean;
@@ -23,6 +25,7 @@ import com.ja.assets.new_code.http.JuanCallback;
 import com.ja.assets.new_code.util.DialogUtil;
 import com.ja.assets.new_code.util.ToastUtil;
 import com.ja.assets.new_code.view.JuanListView;
+import com.ja.assets.new_code.view.chenjinshi.StatusBarUtil;
 import com.ja.assets.new_code.view.refresh.MaterialDesignPtrFrameLayout;
 import com.ja.assets.utils.ACacheUtil;
 
@@ -47,6 +50,21 @@ public class YipandianActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //沉浸式代码配置
+        //当FitsSystemWindows设置 true 时，会在屏幕最上方预留出状态栏高度的 padding
+        StatusBarUtil.setRootViewFitsSystemWindows(this, true);
+        //设置状态栏透明
+        StatusBarUtil.setTranslucentStatus(this);
+        //一般的手机的状态栏文字和图标都是白色的, 可如果你的应用也是纯白色的, 或导致状态栏文字看不清
+        //所以如果你是这种情况,请使用以下代码, 设置状态使用深色文字图标风格, 否则你可以选择性注释掉这个if内容
+        if (!StatusBarUtil.setStatusBarDarkTheme(this, true)) {
+            //如果不支持设置深色风格 为了兼容总不能让状态栏白白的看不清, 于是设置一个状态栏颜色为半透明,
+            //这样半透明+白=灰, 状态栏的文字能看得清
+            StatusBarUtil.setStatusBarColor(this, 0x55000000);
+        }
+        //用来设置整体下移，状态栏沉浸
+        StatusBarUtil.setRootViewFitsSystemWindows(this, false);
         setContentView(R.layout.activity_yipandian);
         iv_back = findViewById(R.id.iv_back);
         iv_back.setOnClickListener(new View.OnClickListener() {
